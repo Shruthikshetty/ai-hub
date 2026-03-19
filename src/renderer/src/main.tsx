1e1b87393fb66c41d6b6eb3c9d6c40ab437656df
shruthik shetty <shrutikshetty14@gmail.com>
2026-03-19 21:40:29 +0530

    merge conflict

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -2,16 +2,10 @@ import './global.css'
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router'
 import App from './app'
 
-/**
- * main entry point of the application
- */
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </StrictMode>
 )
